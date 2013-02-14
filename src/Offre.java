5b4d00c42dce3e0057b1c30d4db3a7ef34cdb61f
Yohann B <yohann.berthon@etu.univ-savoie.fr>
2013-02-14 09:21:19 +0100

    commit

--- src/Offre.java
+++ src/Offre.java
@@ -5,4 +5,29 @@ public class Offre {
 	private String description;
 	
 	
+	
+	
+	public String getUrlImage() {
+		return urlImage;
+	}
+	
+	public void setUrlImage(String urlImage) {
+		this.urlImage = urlImage;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
